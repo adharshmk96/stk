5523fd0f47072dd4e857af0383c05c30332eade4
Adharsh Manikandan <adharsh.manikandan@agoda.com>
2023-10-18 14:07:47 +0700

    fix: template updated with checkout fix.

--- pkg/progen/tpl/project.go
+++ pkg/progen/tpl/project.go
@@ -1,5 +1,27 @@
 package tpl
 
+var GITIGNORE_TPL = Template{
+	FilePath: ".gitignore",
+	Content: `# If you prefer the allow list template instead of the deny list, see community template:
+# https://github.com/github/gitignore/blob/main/community/Golang/Go.AllowList.gitignore
+#
+# Binaries for programs and plugins
+*.exe
+*.exe~
+*.dll
+*.so
+*.dylib
+
+# Test binary
+*.test
+
+# Output of the go coverage tool, specifically when used with LiteIDE
+*.out
+dist
+
+*.db`,
+}
+
 var VERSIONYAML_TPL = Template{
 	FilePath: ".version.yaml",
 	Content: `alpha: 0
@@ -8,6 +30,18 @@ major: 0
 minor: 0
 patch: 0
 rc: 0
+`,
+}
+
+var MAINGO_TPL = Template{
+	FilePath: "main.go",
+	Content: `package main
+
+import "{{ .PkgName }}/cmd"
+
+func main() {
+	cmd.Execute()
+}
 `,
 }
 
@@ -82,45 +116,6 @@ init:
 `,
 }
 
-var GITIGNORE_TPL = Template{
-	FilePath: ".gitignore",
-	Content: `# If you prefer the allow list template instead of the deny list, see community template:
-# https://github.com/github/gitignore/blob/main/community/Golang/Go.AllowList.gitignore
-#
-# Binaries for programs and plugins
-*.exe
-*.exe~
-*.dll
-*.so
-*.dylib
-
-# Test binary
-*.test
-
-# Output of the go coverage tool, specifically when used with LiteIDE
-*.out
-dist
-
-*.db`,
-}
-
-var REQUESTHTTP_TPL = Template{
-	FilePath: "request.http",
-	Content: `GET http://localhost:8080/{{ .ModName }}`,
-}
-
-var MAINGO_TPL = Template{
-	FilePath: "main.go",
-	Content: `package main
-
-import "{{ .PkgName }}/cmd"
-
-func main() {
-	cmd.Execute()
-}
-`,
-}
-
 var READMEMD_TPL = Template{
 	FilePath: "README.md",
 	Content: `# {{ .AppName }}
@@ -180,6 +175,31 @@ The server directory encompasses various elements related to the server-side of 
 --- 
 
 For testing, [mockery](https://github.com/vektra/mockery) is reccomended.`,
+}
+
+var REQUESTHTTP_TPL = Template{
+	FilePath: "request.http",
+	Content: `GET http://localhost:8080/{{ .ModName }}`,
+}
+
+var VSCODE_LAUNCHJSON_TPL = Template{
+	FilePath: ".vscode/launch.json",
+	Content: `{
+    // Use IntelliSense to learn about possible attributes.
+    // Hover to view descriptions of existing attributes.
+    // For more information, visit: https://go.microsoft.com/fwlink/?linkid=830387
+    "version": "0.2.0",
+    "configurations": [
+        {
+            "name": "serve in port 8080",
+            "type": "go",
+            "request": "launch",
+            "mode": "auto",
+            "program": "${workspaceFolder}/main.go",
+            "args": ["serve", "-p", "8080"]
+          }
+    ]
+}`,
 }
 
 var CMD_ROOTGO_TPL = Template{
@@ -323,197 +343,145 @@ func init() {
 `,
 }
 
-var VSCODE_LAUNCHJSON_TPL = Template{
-	FilePath: ".vscode/launch.json",
-	Content: `{
-    // Use IntelliSense to learn about possible attributes.
-    // Hover to view descriptions of existing attributes.
-    // For more information, visit: https://go.microsoft.com/fwlink/?linkid=830387
-    "version": "0.2.0",
-    "configurations": [
-        {
-            "name": "serve in port 8080",
-            "type": "go",
-            "request": "launch",
-            "mode": "auto",
-            "program": "${workspaceFolder}/main.go",
-            "args": ["serve", "-p", "8080"]
-          }
-    ]
-}`,
+var INTERNALS_CORE_ENTITY_PINGGO_TPL = Template{
+	FilePath: "internals/core/entity/ping.go",
+	Content: `package entity
+
+import "github.com/adharshmk96/stk/gsk"
+
+// Domain
+type {{ .ExportedName }}Data struct {
+	{{ .ModName }} string
 }
 
-var SERVER_SETUPGO_TPL = Template{
-	FilePath: "server/setup.go",
-	Content: `package server
+// Storage
+type {{ .ExportedName }}Storage interface {
+	{{ .ExportedName }}() error
+}
+
+// Service
+type {{ .ExportedName }}Service interface {
+	{{ .ExportedName }}Service() (string, error)
+}
+
+// Handler
+type {{ .ExportedName }}Handlers interface {
+	{{ .ExportedName }}Handler(gc *gsk.Context)
+}
+`,
+}
+
+var INTERNALS_CORE_SERR_PINGGO_TPL = Template{
+	FilePath: "internals/core/serr/ping.go",
+	Content: `package serr
+
+import "errors"
+
+var (
+	Err{{ .ExportedName }}Failed = errors.New("{{ .ModName }} failed")
+)
+`,
+}
+
+var INTERNALS_HTTP_HANDLER_PINGGO_TPL = Template{
+	FilePath: "internals/http/handler/ping.go",
+	Content: `package handler
 
 import (
-	"os"
-	"os/signal"
-	"syscall"
+	"net/http"
 
-	"{{ .PkgName }}/server/infra"
-	svrmw "{{ .PkgName }}/server/middleware"
-	"{{ .PkgName }}/server/routing"
+	"{{ .PkgName }}/internals/core/entity"
 	"github.com/adharshmk96/stk/gsk"
-	"github.com/adharshmk96/stk/pkg/middleware"
 )
 
-func StartHttpServer(port string) (*gsk.Server, chan bool) {
+type {{ .ModName }}Handler struct {
+	service entity.{{ .ExportedName }}Service
+}
 
-	logger := infra.GetLogger()
+func New{{ .ExportedName }}Handler(service entity.{{ .ExportedName }}Service) entity.{{ .ExportedName }}Handlers {
+	return &{{ .ModName }}Handler{
+		service: service,
+	}
+}
 
-	serverConfig := &gsk.ServerConfig{
-		Port:   port,
-		Logger: logger,
+/*
+{{ .ExportedName }}Handler returns {{ .ModName }} 200 response
+Response:
+- 200: OK
+- 500: Internal Server Error
+*/
+func (h *{{ .ModName }}Handler) {{ .ExportedName }}Handler(gc *gsk.Context) {
+
+	{{ .ModName }}, err := h.service.{{ .ExportedName }}Service()
+	if err != nil {
+		gc.Status(http.StatusInternalServerError).JSONResponse(gsk.Map{
+			"error": err.Error(),
+		})
+		return
 	}
 
-	server := gsk.New(serverConfig)
-
-	rateLimiter := svrmw.RateLimiter()
-	server.Use(rateLimiter)
-	server.Use(middleware.RequestLogger)
-	server.Use(middleware.CORS(middleware.CORSConfig{
-		AllowAll: true,
-	}))
-
-	infra.LoadDefaultConfig()
-
-	routing.SetupRoutes(server)
-
-	server.Start()
-
-	// graceful shutdown
-	done := make(chan bool)
-
-	// A go routine that listens for os signals
-	// it will block until it receives a signal
-	// once it receives a signal, it will shutdown close the done channel
-	go func() {
-		sigint := make(chan os.Signal, 1)
-		signal.Notify(sigint, os.Interrupt, syscall.SIGTERM)
-		<-sigint
-
-		if err := server.Shutdown(); err != nil {
-			logger.Error(err.Error())
-		}
-
-		close(done)
-	}()
-
-	return server, done
+	gc.Status(http.StatusOK).JSONResponse(gsk.Map{
+		"message": {{ .ModName }},
+	})
 }
 `,
 }
 
-var SERVER_ROUTING_INITROUTESGO_TPL = Template{
-	FilePath: "server/routing/initRoutes.go",
-	Content: `package routing
+var INTERNALS_HTTP_HANDLER_TEST_PING_TESTGO_TPL = Template{
+	FilePath: "internals/http/handler_test/ping_test.go",
+	Content: `package handler_test
+
+// run the following command to generate mocks for {{ .ExportedName }} interfaces
+//
+// mockery --dir=internals/core/entity --name=^{{ .ExportedName }}.*
+//
+// and uncomment the following code
+
+/*
 
 import (
-	"github.com/adharshmk96/stk/gsk"
-)
+	"net/http"
+	"testing"
 
-func SetupRoutes(server *gsk.Server) {
-	setup{{ .ExportedName }}Routes(server)
-}
-`,
-}
-
-var SERVER_ROUTING_PINGGO_TPL = Template{
-	FilePath: "server/routing/ping.go",
-	Content: `package routing
-
-import (
 	"{{ .PkgName }}/internals/http/handler"
-	"{{ .PkgName }}/internals/service"
-	"{{ .PkgName }}/internals/storage/{{ .ModName }}Storage"
-	"{{ .PkgName }}/server/infra"
+	"{{ .PkgName }}/mocks"
 	"github.com/adharshmk96/stk/gsk"
-	"github.com/adharshmk96/stk/pkg/db"
-	"github.com/spf13/viper"
+	"github.com/stretchr/testify/assert"
 )
 
-func setup{{ .ExportedName }}Routes(server *gsk.Server) {
-	dbConfig := viper.GetString(infra.ENV_SQLITE_FILEPATH)
-	conn := db.GetSqliteConnection(dbConfig)
+func Test{{ .ExportedName }}Handler(t *testing.T) {
+	t.Run("{{ .ExportedName }} Handler returns 200", func(t *testing.T) {
 
-	{{ .ModName }}Storage := {{ .ModName }}Storage.NewSqliteRepo(conn)
-	{{ .ModName }}Service := service.New{{ .ExportedName }}Service({{ .ModName }}Storage)
-	{{ .ModName }}Handler := handler.New{{ .ExportedName }}Handler({{ .ModName }}Service)
+		// Arrange
+		s := gsk.New()
+		service := mocks.New{{ .ExportedName }}Service(t)
+		service.On("{{ .ExportedName }}Service").Return("pong", nil)
 
-	server.Get("/{{ .ModName }}", {{ .ModName }}Handler.{{ .ExportedName }}Handler)
+		{{ .ModName }}Handler := handler.New{{ .ExportedName }}Handler(service)
+
+		s.Get("/{{ .ModName }}", {{ .ModName }}Handler.{{ .ExportedName }}Handler)
+
+		// Act
+		w, _ := s.Test("GET", "/{{ .ModName }}", nil)
+
+		// Assert
+		assert.Equal(t, http.StatusOK, w.Code)
+	})
 }
+
+*/
 `,
 }
 
-var SERVER_MIDDLEWARE_MIDDLEWAREGO_TPL = Template{
-	FilePath: "server/middleware/middleware.go",
-	Content: `package middleware
-
-import (
-	"time"
-
-	"github.com/adharshmk96/stk/gsk"
-	gskmw "github.com/adharshmk96/stk/pkg/middleware"
-)
-
-func RateLimiter() gsk.Middleware {
-	rlConfig := gskmw.RateLimiterConfig{
-		RequestsPerInterval: 10,
-		Interval:            60 * time.Second,
-	}
-	rateLimiter := gskmw.NewRateLimiter(rlConfig)
-	return rateLimiter.Middleware
-}
+var INTERNALS_HTTP_HELPERS_PINGGO_TPL = Template{
+	FilePath: "internals/http/helpers/ping.go",
+	Content: `package helpers
 `,
 }
 
-var SERVER_INFRA_CONFIGGO_TPL = Template{
-	FilePath: "server/infra/config.go",
-	Content: `package infra
-
-import "github.com/spf13/viper"
-
-// Configurations are loaded from the environment variables using viper.
-// callin this function will reLoad the config. (useful for testing)
-// WARN: this will reload all the config.
-func LoadDefaultConfig() {
-	viper.SetDefault(ENV_SQLITE_FILEPATH, "database.db")
-
-	viper.AutomaticEnv()
-}
-`,
-}
-
-var SERVER_INFRA_CONSTANTSGO_TPL = Template{
-	FilePath: "server/infra/constants.go",
-	Content: `package infra
-
-const (
-	ENV_SQLITE_FILEPATH = "SQLITE_FILEPATH"
-)
-`,
-}
-
-var SERVER_INFRA_LOGGERGO_TPL = Template{
-	FilePath: "server/infra/logger.go",
-	Content: `package infra
-
-import (
-	"log/slog"
-	"os"
-)
-
-var logger *slog.Logger
-
-func init() {
-	logger = slog.New(slog.NewJSONHandler(os.Stdout, nil))
-}
-
-func GetLogger() *slog.Logger {
-	return logger
-}
+var INTERNALS_HTTP_TRANSPORT_PINGGO_TPL = Template{
+	FilePath: "internals/http/transport/ping.go",
+	Content: `package transport
 `,
 }
 
@@ -585,148 +553,6 @@ func Test{{ .ExportedName }}Service(t *testing.T) {
 `,
 }
 
-var INTERNALS_CORE_SERR_PINGGO_TPL = Template{
-	FilePath: "internals/core/serr/ping.go",
-	Content: `package serr
-
-import "errors"
-
-var (
-	Err{{ .ExportedName }}Failed = errors.New("{{ .ModName }} failed")
-)
-`,
-}
-
-var INTERNALS_CORE_ENTITY_PINGGO_TPL = Template{
-	FilePath: "internals/core/entity/ping.go",
-	Content: `package entity
-
-import "github.com/adharshmk96/stk/gsk"
-
-// Domain
-type {{ .ExportedName }}Data struct {
-	{{ .ModName }} string
-}
-
-// Storage
-type {{ .ExportedName }}Storage interface {
-	{{ .ExportedName }}() error
-}
-
-// Service
-type {{ .ExportedName }}Service interface {
-	{{ .ExportedName }}Service() (string, error)
-}
-
-// Handler
-type {{ .ExportedName }}Handlers interface {
-	{{ .ExportedName }}Handler(gc *gsk.Context)
-}
-`,
-}
-
-var INTERNALS_HTTP_HELPERS_PINGGO_TPL = Template{
-	FilePath: "internals/http/helpers/ping.go",
-	Content: `package helpers
-`,
-}
-
-var INTERNALS_HTTP_HANDLER_PINGGO_TPL = Template{
-	FilePath: "internals/http/handler/ping.go",
-	Content: `package handler
-
-import (
-	"net/http"
-
-	"{{ .PkgName }}/internals/core/entity"
-	"github.com/adharshmk96/stk/gsk"
-)
-
-type {{ .ModName }}Handler struct {
-	service entity.{{ .ExportedName }}Service
-}
-
-func New{{ .ExportedName }}Handler(service entity.{{ .ExportedName }}Service) entity.{{ .ExportedName }}Handlers {
-	return &{{ .ModName }}Handler{
-		service: service,
-	}
-}
-
-/*
-{{ .ExportedName }}Handler returns {{ .ModName }} 200 response
-Response:
-- 200: OK
-- 500: Internal Server Error
-*/
-func (h *{{ .ModName }}Handler) {{ .ExportedName }}Handler(gc *gsk.Context) {
-
-	{{ .ModName }}, err := h.service.{{ .ExportedName }}Service()
-	if err != nil {
-		gc.Status(http.StatusInternalServerError).JSONResponse(gsk.Map{
-			"error": err.Error(),
-		})
-		return
-	}
-
-	gc.Status(http.StatusOK).JSONResponse(gsk.Map{
-		"message": {{ .ModName }},
-	})
-}
-`,
-}
-
-var INTERNALS_HTTP_TRANSPORT_PINGGO_TPL = Template{
-	FilePath: "internals/http/transport/ping.go",
-	Content: `package transport
-`,
-}
-
-var INTERNALS_HTTP_HANDLER_TEST_PING_TESTGO_TPL = Template{
-	FilePath: "internals/http/handler_test/ping_test.go",
-	Content: `package handler_test
-
-// run the following command to generate mocks for {{ .ExportedName }} interfaces
-//
-// mockery --dir=internals/core/entity --name=^{{ .ExportedName }}.*
-//
-// and uncomment the following code
-
-/*
-
-import (
-	"net/http"
-	"testing"
-
-	"{{ .PkgName }}/internals/http/handler"
-	"{{ .PkgName }}/mocks"
-	"github.com/adharshmk96/stk/gsk"
-	"github.com/stretchr/testify/assert"
-)
-
-func Test{{ .ExportedName }}Handler(t *testing.T) {
-	t.Run("{{ .ExportedName }} Handler returns 200", func(t *testing.T) {
-
-		// Arrange
-		s := gsk.New()
-		service := mocks.New{{ .ExportedName }}Service(t)
-		service.On("{{ .ExportedName }}Service").Return("pong", nil)
-
-		{{ .ModName }}Handler := handler.New{{ .ExportedName }}Handler(service)
-
-		s.Get("/{{ .ModName }}", {{ .ModName }}Handler.{{ .ExportedName }}Handler)
-
-		// Act
-		w, _ := s.Test("GET", "/{{ .ModName }}", nil)
-
-		// Assert
-		assert.Equal(t, http.StatusOK, w.Code)
-	})
-}
-
-*/
-`,
-}
-
 var INTERNALS_STORAGE_PINGSTORAGE_PINGGO_TPL = Template{
 	FilePath: "internals/storage/pingStorage/ping.go",
 	Content: `package {{ .ModName }}Storage
@@ -788,33 +614,207 @@ const (
 `,
 }
 
+var SERVER_SETUPGO_TPL = Template{
+	FilePath: "server/setup.go",
+	Content: `package server
+
+import (
+	"os"
+	"os/signal"
+	"syscall"
+
+	"{{ .PkgName }}/server/infra"
+	svrmw "{{ .PkgName }}/server/middleware"
+	"{{ .PkgName }}/server/routing"
+	"github.com/adharshmk96/stk/gsk"
+	"github.com/adharshmk96/stk/pkg/middleware"
+)
+
+func StartHttpServer(port string) (*gsk.Server, chan bool) {
+
+	logger := infra.GetLogger()
+
+	serverConfig := &gsk.ServerConfig{
+		Port:   port,
+		Logger: logger,
+	}
+
+	server := gsk.New(serverConfig)
+
+	rateLimiter := svrmw.RateLimiter()
+	server.Use(rateLimiter)
+	server.Use(middleware.RequestLogger)
+	server.Use(middleware.CORS(middleware.CORSConfig{
+		AllowAll: true,
+	}))
+
+	infra.LoadDefaultConfig()
+
+	routing.SetupRoutes(server)
+
+	server.Start()
+
+	// graceful shutdown
+	done := make(chan bool)
+
+	// A go routine that listens for os signals
+	// it will block until it receives a signal
+	// once it receives a signal, it will shutdown close the done channel
+	go func() {
+		sigint := make(chan os.Signal, 1)
+		signal.Notify(sigint, os.Interrupt, syscall.SIGTERM)
+		<-sigint
+
+		if err := server.Shutdown(); err != nil {
+			logger.Error(err.Error())
+		}
+
+		close(done)
+	}()
+
+	return server, done
+}
+`,
+}
+
+var SERVER_INFRA_CONFIGGO_TPL = Template{
+	FilePath: "server/infra/config.go",
+	Content: `package infra
+
+import "github.com/spf13/viper"
+
+// Configurations are loaded from the environment variables using viper.
+// callin this function will reLoad the config. (useful for testing)
+// WARN: this will reload all the config.
+func LoadDefaultConfig() {
+	viper.SetDefault(ENV_SQLITE_FILEPATH, "database.db")
+
+	viper.AutomaticEnv()
+}
+`,
+}
+
+var SERVER_INFRA_CONSTANTSGO_TPL = Template{
+	FilePath: "server/infra/constants.go",
+	Content: `package infra
+
+const (
+	ENV_SQLITE_FILEPATH = "SQLITE_FILEPATH"
+)
+`,
+}
+
+var SERVER_INFRA_LOGGERGO_TPL = Template{
+	FilePath: "server/infra/logger.go",
+	Content: `package infra
+
+import (
+	"log/slog"
+	"os"
+)
+
+var logger *slog.Logger
+
+func init() {
+	logger = slog.New(slog.NewJSONHandler(os.Stdout, nil))
+}
+
+func GetLogger() *slog.Logger {
+	return logger
+}
+`,
+}
+
+var SERVER_MIDDLEWARE_MIDDLEWAREGO_TPL = Template{
+	FilePath: "server/middleware/middleware.go",
+	Content: `package middleware
+
+import (
+	"time"
+
+	"github.com/adharshmk96/stk/gsk"
+	gskmw "github.com/adharshmk96/stk/pkg/middleware"
+)
+
+func RateLimiter() gsk.Middleware {
+	rlConfig := gskmw.RateLimiterConfig{
+		RequestsPerInterval: 10,
+		Interval:            60 * time.Second,
+	}
+	rateLimiter := gskmw.NewRateLimiter(rlConfig)
+	return rateLimiter.Middleware
+}
+`,
+}
+
+var SERVER_ROUTING_INITROUTESGO_TPL = Template{
+	FilePath: "server/routing/initRoutes.go",
+	Content: `package routing
+
+import (
+	"github.com/adharshmk96/stk/gsk"
+)
+
+func SetupRoutes(server *gsk.Server) {
+	setup{{ .ExportedName }}Routes(server)
+}
+`,
+}
+
+var SERVER_ROUTING_PINGGO_TPL = Template{
+	FilePath: "server/routing/ping.go",
+	Content: `package routing
+
+import (
+	"{{ .PkgName }}/internals/http/handler"
+	"{{ .PkgName }}/internals/service"
+	"{{ .PkgName }}/internals/storage/{{ .ModName }}Storage"
+	"{{ .PkgName }}/server/infra"
+	"github.com/adharshmk96/stk/gsk"
+	"github.com/adharshmk96/stk/pkg/db"
+	"github.com/spf13/viper"
+)
+
+func setup{{ .ExportedName }}Routes(server *gsk.Server) {
+	dbConfig := viper.GetString(infra.ENV_SQLITE_FILEPATH)
+	conn := db.GetSqliteConnection(dbConfig)
+
+	{{ .ModName }}Storage := {{ .ModName }}Storage.NewSqliteRepo(conn)
+	{{ .ModName }}Service := service.New{{ .ExportedName }}Service({{ .ModName }}Storage)
+	{{ .ModName }}Handler := handler.New{{ .ExportedName }}Handler({{ .ModName }}Service)
+
+	server.Get("/{{ .ModName }}", {{ .ModName }}Handler.{{ .ExportedName }}Handler)
+}
+`,
+}
+
 var ProjectTemplates = []Template{
-	VERSIONYAML_TPL,
-	MAKEFILE_TPL,
 	GITIGNORE_TPL,
-	REQUESTHTTP_TPL,
+	VERSIONYAML_TPL,
 	MAINGO_TPL,
+	MAKEFILE_TPL,
 	READMEMD_TPL,
+	REQUESTHTTP_TPL,
+	VSCODE_LAUNCHJSON_TPL,
 	CMD_ROOTGO_TPL,
 	CMD_SERVEGO_TPL,
 	CMD_VERSIONGO_TPL,
-	VSCODE_LAUNCHJSON_TPL,
-	SERVER_SETUPGO_TPL,
-	SERVER_ROUTING_INITROUTESGO_TPL,
-	SERVER_ROUTING_PINGGO_TPL,
-	SERVER_MIDDLEWARE_MIDDLEWAREGO_TPL,
-	SERVER_INFRA_CONFIGGO_TPL,
-	SERVER_INFRA_CONSTANTSGO_TPL,
-	SERVER_INFRA_LOGGERGO_TPL,
+	INTERNALS_CORE_ENTITY_PINGGO_TPL,
+	INTERNALS_CORE_SERR_PINGGO_TPL,
+	INTERNALS_HTTP_HANDLER_PINGGO_TPL,
+	INTERNALS_HTTP_HANDLER_TEST_PING_TESTGO_TPL,
+	INTERNALS_HTTP_HELPERS_PINGGO_TPL,
+	INTERNALS_HTTP_TRANSPORT_PINGGO_TPL,
 	INTERNALS_SERVICE_PINGGO_TPL,
 	INTERNALS_SERVICE_TEST_PING_TESTGO_TPL,
-	INTERNALS_CORE_SERR_PINGGO_TPL,
-	INTERNALS_CORE_ENTITY_PINGGO_TPL,
-	INTERNALS_HTTP_HELPERS_PINGGO_TPL,
-	INTERNALS_HTTP_HANDLER_PINGGO_TPL,
-	INTERNALS_HTTP_TRANSPORT_PINGGO_TPL,
-	INTERNALS_HTTP_HANDLER_TEST_PING_TESTGO_TPL,
 	INTERNALS_STORAGE_PINGSTORAGE_PINGGO_TPL,
 	INTERNALS_STORAGE_PINGSTORAGE_PINGCONNECTIONGO_TPL,
 	INTERNALS_STORAGE_PINGSTORAGE_PINGQUERIESGO_TPL,
+	SERVER_SETUPGO_TPL,
+	SERVER_INFRA_CONFIGGO_TPL,
+	SERVER_INFRA_CONSTANTSGO_TPL,
+	SERVER_INFRA_LOGGERGO_TPL,
+	SERVER_MIDDLEWARE_MIDDLEWAREGO_TPL,
+	SERVER_ROUTING_INITROUTESGO_TPL,
+	SERVER_ROUTING_PINGGO_TPL,
 }
