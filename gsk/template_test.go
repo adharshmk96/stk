0573c37933600add192b423aec8150663f46765b
Adharsh M <dev@adharsh.in>
2023-10-22 23:48:53 +0700

    refactor: test improvment.

--- gsk/template_test.go
+++ gsk/template_test.go
@@ -5,75 +5,44 @@ import (
 	"testing"
 
 	"github.com/adharshmk96/stk/gsk"
-	"github.com/adharshmk96/stk/testutils"
 	"github.com/stretchr/testify/assert"
 )
 
-var templateContent = `<html>
-	<head>
-		<title>{{ .Title }}</title>
-	</head>
-	<body>
-		<h1>{{ .Title }}</h1>
-		<p>{{ .Body }}</p>
-	</body>
-</html>`
+// Helper function to create temporary test template files.
+func createTestFile(name string, content string) {
+	f, _ := os.Create(name)
+	defer f.Close()
+	f.WriteString(content)
+}
 
-func TestTemplateResponseRender(t *testing.T) {
-	t.Run("renders template with data", func(t *testing.T) {
-		tempFile, removeDir := testutils.CreateTempFile(t, templateContent)
-		defer removeDir()
+func TestMain(m *testing.M) {
+	// Create temporary template files
+	createTestFile("success_template.html", "Hello, {{.Name}}!")
+	createTestFile("failure_template.html", "Hello, {{.Name!")
+	// Run tests
+	code := m.Run()
+	// Cleanup
+	os.Remove("success_template.html")
+	os.Remove("failure_template.html")
+	// Exit
+	os.Exit(code)
+}
 
-		data := struct {
-			Title string
-			Body  string
-		}{
-			Title: "Hello",
-			Body:  "World",
-		}
+func TestRender(t *testing.T) {
+	// Successful rendering scenario
+	tpl := &gsk.Tpl{
+		TemplatePath: "success_template.html",
+		Variables:    map[string]string{"Name": "World"},
+	}
+	data, err := tpl.Render()
+	assert.NoError(t, err)
+	assert.Equal(t, "Hello, World!", string(data))
 
-		templateResp := gsk.Tpl{
-			TemplatePath: tempFile,
-			Variables:    data,
-		}
+	// Parsing error scenario
+	tpl = &gsk.Tpl{
+		TemplatePath: "failure_template.html",
+	}
+	_, err = tpl.Render()
+	assert.Error(t, err)
 
-		rendered, err := templateResp.Render()
-		assert.NoError(t, err)
-
-		expected := `<html>
-	<head>
-		<title>Hello</title>
-	</head>
-	<body>
-		<h1>Hello</h1>
-		<p>World</p>
-	</body>
-</html>`
-
-		assert.Equal(t, expected, string(rendered))
-	})
-
-	t.Run("execute error is returned", func(t *testing.T) {
-		tempFile, _ := testutils.CreateTempFile(t, templateContent)
-
-		data := struct {
-			Title string
-			Body  string
-		}{
-			Title: "Hello",
-			Body:  "World",
-		}
-
-		templateResp := gsk.Tpl{
-			TemplatePath: tempFile,
-			Variables:    data,
-		}
-
-		// Remove template file to force execute error
-		err := os.Remove(tempFile)
-		assert.NoError(t, err)
-
-		_, err = templateResp.Render()
-		assert.Error(t, err)
-	})
 }
